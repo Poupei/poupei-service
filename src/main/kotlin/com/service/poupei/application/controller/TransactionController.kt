eaee3e0318bc26e2fb855652183bdcffe1df1845
yanhudson <yan.hudson@bandtec.com.br>
2024-04-22 00:26:54 -0300

    feat: documenting the user controller and transaction controller

--- src/main/kotlin/com/service/poupei/application/controller/TransactionController.kt
+++ src/main/kotlin/com/service/poupei/application/controller/TransactionController.kt
@@ -3,6 +3,10 @@ package com.service.poupei.application.controller
 import com.service.poupei.application.controller.dto.CreateTransactionDto
 import com.service.poupei.application.controller.dto.TransactionDto
 import com.service.poupei.application.usecase.transaction.*
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 
+@Tag(name = "Transaction", description = "Transaction controller that is reponsible to do things about financial movement")
 @Controller
 @RequestMapping("/transactions")
 class TransactionController(
@@ -24,25 +29,54 @@ class TransactionController(
         private val retrieveTransactionUseCase: RetrieveTransactionUseCase
 ) {
 
+
+    @Operation(
+        summary = "Retrieve all the transactions",
+        responses = [
+            ApiResponse(responseCode = "200", description = "Successful in retrieve all transactions"),
+            ApiResponse(responseCode = "204", description = "It doesn't find any transaction")
+        ],
+    )
     @GetMapping
     fun retrieveAll(): ResponseEntity<List<TransactionDto>> =
             ResponseEntity.ok(retrieveAllTransactionsUseCase.all().map { transactionModel ->
                 TransactionDto.from(transactionModel)
             })
 
+    @Operation(
+        summary = "Retrieve a specific transaction information",
+        responses = [
+            ApiResponse(responseCode = "200", description = "Successful transaction recovery"),
+            ApiResponse(responseCode = "404", description = "Not found the transaction")
+        ]
+    )
     @GetMapping("/{id}")
     fun retrieveWith(@PathVariable id: String): ResponseEntity<TransactionDto> =
             ResponseEntity.ok(TransactionDto.from(
                     retrieveTransactionUseCase.with(id)
             ))
 
+    @Operation(
+        summary = "Adding a transaction",
+        responses = [
+            ApiResponse(responseCode = "200", description = "Successful to add transaction"),
+            ApiResponse(responseCode = "400", description = "Something is wrong with the field types")
+        ],
+    )
     @PostMapping
     fun createWith(@RequestBody @Valid createTransactionDto: CreateTransactionDto): ResponseEntity<TransactionDto> =
             ResponseEntity.ok(TransactionDto.from(
                     createTransactionUseCase.with(createTransactionDto.toModel())
             ))
 
-
+    @Operation(
+        summary = "Update a specific transaction",
+        responses = [
+            ApiResponse(responseCode = "200", description = "successful in updating the transaction"),
+            ApiResponse(responseCode = "204", description = "Successful in request, but nothing it was update"),
+            ApiResponse(responseCode = "404", description = "Not found the transaction")
+        ]
+    )
     @PutMapping("/{id}")
     fun updateWith(
             @PathVariable id: String,
@@ -53,7 +87,13 @@ class TransactionController(
             ))
 
 
-
+    @Operation(
+        summary = "Delete a transaction",
+        responses = [
+            ApiResponse(responseCode = "200", description = "Successful to delete the transaction"),
+            ApiResponse(responseCode = "404", description = "Not found the transaction")
+        ]
+    )
     @DeleteMapping("/{id}")
     fun deleteWith(@PathVariable id: String): ResponseEntity<TransactionDto> =
             ResponseEntity.ok(TransactionDto.from(deleteTransactionUseCase.with(id)))
--- src/main/kotlin/com/service/poupei/application/controller/UserController.kt
+++ src/main/kotlin/com/service/poupei/application/controller/UserController.kt
@@ -4,10 +4,14 @@ import com.service.poupei.application.controller.dto.CreateUserDto
 import com.service.poupei.application.controller.dto.UpdateUserDto
 import com.service.poupei.application.controller.dto.UserDto
 import com.service.poupei.application.usecase.user.*
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 
+@Tag(name = "Users", description = "User controller that is reponsible to do things to related to users")
 @Controller
 @RequestMapping("/users")
 class UserController(
@@ -18,25 +22,60 @@ class UserController(
     private val retrieveUserCase: RetrieveUserCase
 ) {
 
-
+    @Operation(
+        summary = "Retrieve all the user",
+        responses = [
+            ApiResponse(responseCode = "200", description = "Successful in retrieve all users"),
+            ApiResponse(responseCode = "204", description = "It doesn't find any users")
+        ],
+    )
     @GetMapping
     fun retrieveAll() : ResponseEntity<List<UserDto>> =
         ResponseEntity.ok(retrieveAllUserUseCase.all().map {
             UserDto.from(it)
         })
 
+    @Operation(
+        summary = "Retrieve a specific user information",
+        responses = [
+            ApiResponse(responseCode = "200", description = "Successful user recovery"),
+            ApiResponse(responseCode = "404", description = "Not found the user")
+        ]
+    )
     @GetMapping("/{id}")
     fun retrieveWith(@PathVariable id: String) : ResponseEntity<UserDto> =
         ResponseEntity.ok(UserDto.from(retrieveUserCase.with(id)));
 
+    @Operation(
+        summary = "Creating a user",
+        responses = [
+            ApiResponse(responseCode = "200", description = "Successful to create a user"),
+            ApiResponse(responseCode = "400", description = "Something is wrong with the field types")
+        ],
+    )
     @PostMapping
     fun createWith(@RequestBody createUserDto: CreateUserDto) : ResponseEntity<UserDto> =
         ResponseEntity.ok(UserDto.from(createUserUseCase.with(createUserDto.toModel())));
 
+    @Operation(
+        summary = "Update a specific user information",
+        responses = [
+            ApiResponse(responseCode = "200", description = "successful in updating the user information"),
+            ApiResponse(responseCode = "204", description = "Successful in request, but nothing it was update"),
+            ApiResponse(responseCode = "404", description = "Not found the user")
+        ]
+    )
     @PutMapping("/{id}")
     fun updateWith(@PathVariable id: String, updateUserDto: UpdateUserDto) : ResponseEntity<UserDto> =
         ResponseEntity.ok(UserDto.from(updateUserUseCase.with(updateUserDto.toModelWith(id))))
 
+    @Operation(
+        summary = "Delete a user",
+        responses = [
+            ApiResponse(responseCode = "200", description = "Successful to delete the user"),
+            ApiResponse(responseCode = "404", description = "Not found the user")
+        ]
+    )
     @DeleteMapping("/{id}")
     fun deleteWith(@PathVariable id: String) : ResponseEntity<UserDto> =
         ResponseEntity.ok(UserDto.from(deleteUserUseCase.with(id)));
